0e12b697e3ac3c6203fcc47df83447fb474831bb
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2023-09-18 21:45:59 +0900

    build-essentialを追加

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+sudo apt install build-essential cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
 git clone https://github.com/raspberrypi/pico-sdk
 cd pico-sdk
 git submodule update --init
